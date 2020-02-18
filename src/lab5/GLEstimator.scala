63617c0c9f4005c6d6eb30535f2aa469441eb3bd
john-craig <john.craig1@marist.edu>
2020-02-18 18:35:44 -0500

    Finished lab 5

---
+++ src/lab5/GLEstimator.scala
@@ -0,0 +1,24 @@
+package lab5
+
+object GLEstimator extends App {
+  val n = Int.MaxValue
+  //val pi = nums.foldLeft(0){(a,b) => (a + ((-1 ^ b) / ((2 * b) + 1)))}
+  def IterationGL(n: Double) = {
+    Math.pow(-1.0, n) / ((2.0 * n) + 1.0)
+  }
+
+  def SummationGL(n: Int): Double = {
+    (0 to n).foldLeft(0.0){(a,b) => (a + IterationGL(b))}
+  }
+
+  val start = System.nanoTime()
+
+  val quarter_pi = SummationGL(n)
+  val pi = quarter_pi * 4.0
+
+  val end = System.nanoTime()
+  val dt = (end - start) / 1000000000
+
+  println("Pi: " + pi)
+  println("dt: " + dt)
+}
